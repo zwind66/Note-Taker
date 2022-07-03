cc7f8929684cc06bc1dcb8cc4c3ecf013d52c298
Shuai Zuo <szuo1@vols.utk.edu>
2022-07-03 15:38:19 -0500

    done routes part

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,19 @@
 // Dependencies
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { loadNotes, addNote, deleteNote } = require('../lib/notes');
 const router = require("express").Router();
 
 // Routes
 router.get("/api/notes", (req, res) => {
-    
+    loadNotes(req,res);
+});
 
+router.post("/api/notes", (req, res) => {   
+    addNote(req,res);
+});
 
-
-
-
-
+router.delete("/api/notes/:id", (req, res) => {
+    deleteNote(req,res);
+});
 
 // Export routes for server.js to use.
 module.exports = router;
